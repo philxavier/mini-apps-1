86487c2f10cca3b2cbe22e0aa4f8bc5de708b2a8
Felipe <phil.sousa@gmail.com>
2019-01-10 15:52:36 -0800

    implemented same functionality using a text box and ajax request

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,67 +1,36 @@
+$(document).ready(function () {
 
-$( document ).ready(function() {
-    
+
     var button = document.getElementById('form_button');
 
-    var text = button.addEventListener("click", () => {  
 
+    button.addEventListener("click", (e) => {
+        e.preventDefault;
+        var input = document.getElementById('inputBox').value;
 
-       var test =  $('#myForm').datafile;
-       console.log(test);
-        // var http = new XMLHttpRequest();
-        // var y = 'json'
-        // var z = document.getElementById("inputBox").value;
-        // var postdata= "text=y&data=z"; //Probably need the escape method for values here, like you did
-        
-        // http.open("POST", "/", true);
     
-        // //Send the proper header information along with the request
-        // http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-        // http.setRequestHeader("Content-length", postdata.length);
-    
-        // http.onreadystatechange = function() {//Call a function when the state changes.
-        //     if(http.readyState == 4 && http.status == 200) {
-        //         alert(http.responseText);
-        //     }
-        // }
-        // http.send(postdata);
-        // })
-        
-        // var input = document.getElementById("inputBox").value;
-        // var request = new XMLHttpRequest();
-        // request.open('POST', 'what is the dea', true);
-        // request.send();
-
-
-        // request.onreadystate = function() {
-        //     if (request.readyState == request.DONE && request.status == 200) {
-        //        alert(request.responseText);
-        //     }
-        //  };
-     
-
-        // $.post("/", {data:input}, function(result){
-        //     var text = result;
-        //     var paragraphResult = document.createElement("p");
-        //     paragraphResult.innerHTML = text;
-        //     console.log(paragraphResult);
-        //     $('#results').append(paragraphResult);
-        // });
+        $.post("/", {data:input}, function(result){
+            var text = result;
+            var paragraphResult = document.createElement("p");
+            paragraphResult.innerHTML = text;
+            console.log(paragraphResult);
+            $('#results').append(paragraphResult);
+        });
     })
-        
+
 });
 
 
 
-        
-        
-    //     var xmlhttp = new XMLHttpRequest();   // new HttpRequest instance 
-    //     xmlhttp.open("POST","/");
-    //     xmlhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-    //     xmlhttp.send(JSON.stringify({ "email": "hello@user.com", "response": { "name": "Tester" } }))
-        
-    // })
-       
+
+
+//     var xmlhttp = new XMLHttpRequest();   // new HttpRequest instance 
+//     xmlhttp.open("POST","/");
+//     xmlhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+//     xmlhttp.send(JSON.stringify({ "email": "hello@user.com", "response": { "name": "Tester" } }))
+
+// })
+
 
 
 
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 var path = require('path');
-//var parser = require('body-parser')
+var parser = require('body-parser')
 var fs = require('fs');
 var multer  = require('multer')
 const fileUpload = require('express-fileupload');
@@ -9,15 +9,15 @@ var manipulate_data = require('./manipulate_data')
 
 
 
-// app.use(parser.urlencoded({
-//     extended: true
-// }));
+app.use(parser.urlencoded({
+    extended: true
+}));
 
-app.use(fileUpload())
+// app.use(fileUpload())
 
 app.use(express.static('client'));
 
-//app.use(parser.json());
+app.use(parser.json());
 
 app.listen(8080);
 
@@ -28,43 +28,13 @@ app.get('/', function(req, res) {
 
 app.post('/', function (req, res) {
 
-    var messageObj = req.files.file.data.toString()
-   
-    var messageObj =  JSON.parse(messageObj);
+    console.log('req body here==========', req.body)
+    var messageObj = JSON.parse(req.body.data);
     var columns = manipulate_data.getColumns(messageObj);
     var data = manipulate_data.getData(messageObj);
     var result = manipulate_data.buildAnswer(columns, data); 
     console.log('results here=====================', result)
-    res.send(`<!DOCTYPE html>
-        <html lang="en">
-        <head>
-            <meta charset="UTF-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <meta http-equiv="X-UA-Compatible" content="ie=edge">
-            <title>Document</title>
-            <link rel="stylesheet" type="text/css" href="./style.css">
-
-        </head>
-        <body>
-            <h1>JSON to CSV</h1>
-            <form method="post" action= "/" class="mainForm" id="myform" enctype="multipart/form-data">
-                <div class="form-example">
-                    <label for="name">Select the file</label>
-                    <div>
-                        <input type="file" id="filePick" name="file"accept=".doc, .txt">
-                    </div>
-                </div>
-                <div class="form-example">
-                    <button id="form_button" type="submit">Submit</button>
-                </div>
-            </form>
-            <div id="results">${result}</div>
-
-            <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
-            <script type="text/javascript" src="./app.js"></script>
-        </body>
-        </html>`);
+    res.send(result);
 
 });
 
-/////////////////////////////functions
